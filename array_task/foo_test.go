41df193a9d26a5bf9eca57858281f25e57f86c85
CodeIgor31 <yagmurov.igor@mail.ru>
2023-05-11 23:10:36 +0300

    Low diff

---
+++ array_task/foo_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSafeWrite(t *testing.T) {
+	a := assert.New(t)
+	a.Equal([5]int{}, SafeWrite([5]int{}, -1, 10))
+	a.Equal([5]int{}, SafeWrite([5]int{}, -10, 10))
+	a.Equal([5]int{}, SafeWrite([5]int{}, 10, 10))
+	a.Equal([5]int{}, SafeWrite([5]int{}, 20, 10))
+	a.Equal([5]int{}, SafeWrite([5]int{}, 5, 10))
+	a.Equal([5]int{10, 2, 3, 4, 5}, SafeWrite([5]int{1, 2, 3, 4, 5}, 0, 10))
+	a.Equal([5]int{1, 2, 10, 4, 5}, SafeWrite([5]int{1, 2, 3, 4, 5}, 2, 10))
+	a.Equal([5]int{1, 2, 3, 4, 22}, SafeWrite([5]int{1, 2, 3, 4, 5}, 4, 22))
+}

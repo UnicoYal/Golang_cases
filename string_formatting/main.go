69e0c5e99de37cc5c5bd48036b979d5db1c7ac9d
CodeIgor31 <yagmurov.igor@mail.ru>
2023-05-13 12:31:24 +0300

    No diff

---
+++ string_formatting/main.go
@@ -0,0 +1,7 @@
+package stringformatting
+
+import "fmt"
+
+func generateSelfStory(name string, age int, money float64) string {
+	return fmt.Sprintf("Hello! My name is %s. I'm %d y.o. And I also have $%.2f in my wallet right now.", name, age, money)
+}

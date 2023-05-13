07bea599e13f9ef3adfac39e2737c44451fe6ee1
CodeIgor31 <yagmurov.igor@mail.ru>
2023-05-13 11:00:26 +0300

    Up-low diff

---
+++ sort_slice/main.go
@@ -0,0 +1,20 @@
+package sortslice
+
+import "sort"
+
+func UniqueSortedUserIDs(userIDs []int64) []int64 {
+	if len(userIDs) < 2 {
+		return userIDs
+	}
+
+	sort.SliceStable(userIDs, func(i, j int) bool { return userIDs[i] < userIDs[j] })
+	uniqPointer := 0
+	for i := 1; i < len(userIDs); i++ {
+		if userIDs[uniqPointer] != userIDs[i] {
+			uniqPointer++
+			userIDs[uniqPointer] = userIDs[i]
+		}
+	}
+
+	return userIDs[:uniqPointer+1]
+}
